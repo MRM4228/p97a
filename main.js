2d47e86d6399ddd0145c14f22e839aef51e4452e
MRM4228 <94653015+MRM4228@users.noreply.github.com>
2022-02-13 13:16:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var addin1= document.getElementById("add_in1").Text;
+
+var addin2= document.getElementById("add_in2").Text;
+
+var addans1= document.getElementById("add_ans1").Text;
+
+var addans2= document.getElementById("add_ans2").Text;
+
+
+var add= Math.random_number = Math.floor(Math.random() * (addin1 - addin2));
+
+var ans= Math.random_number = Math.floor(Math.random() * (addans1 - addans2));
+
+var add_x = add-ans;
+
+function addstart()
+{
+row = "<h2> x +" + addin1 + "=" + addin2 + "</h2>";
+document.getElementById("output").innerHTML = row;
+}
+
+
+if (document.getElementById("addx"). Text = add_x ){
+    var add=  Math.floor(Math.random() * (addin1 - addin2));
+
+var ans=  Math.floor(Math.random() * (addans1 - addans2));
+
+var add_x = add - ans;
+
+console.log(add)
+console.log(ans)
+}
